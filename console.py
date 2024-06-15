71837bf3c7c0ca21a78c9a15ba7dbb64a92c4b97
Ali Ahmed <ali.ahmed.engineer.442@gmail.com>
2024-06-15 08:37:28 +0300

    A

--- console.py
+++ console.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
-"""This is the console for AirBnB"""
+"""Defines the HBNB console."""
 import cmd
-import shlex
-import json
-import re
-from ast import literal_eval
+from shlex import split
 from models import storage
 from datetime import datetime
 from models.base_model import BaseModel
@@ -14,104 +11,67 @@ from models.city import City
 from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
-from shlex import split
 
 
 class HBNBCommand(cmd.Cmd):
-    """this class is entry point of the command interpreter
-    """
+    """Defines the HolbertonBnB command interpreter."""
+
     prompt = "(hbnb) "
-    all_classes = {"BaseModel": BaseModel, "User": User, "State": State,
-                   "City": City, "Amenity": Amenity, "Place": Place,
-                   "Review": Review}
+    __classes = {
+        "BaseModel",
+        "User",
+        "State",
+        "City",
+        "Amenity",
+        "Place",
+        "Review"
+    }
 
     def emptyline(self):
-        """Ignores empty spaces"""
+        """Ignore empty spaces."""
         pass
 
     def do_quit(self, line):
-        """Quit command to exit the program"""
+        """Quit command to exit the program."""
         return True
 
     def do_EOF(self, line):
-        """Quit command to exit the program at end of file"""
+        """EOF signal to exit the program."""
+        print("")
         return True
 
     def do_create(self, line):
-        """Creates a new instance of BaseModel, saves it
-        Exceptions:
-            SyntaxError: when there is no args given
-            NameError: when there is no object that has the name
+        """Usage: create <class> <key 1>=<value 2> <key 2>=<value 2> ...
+        Create a new class instance with given keys/values and print its id.
         """
         try:
             if not line:
                 raise SyntaxError()
-            # my_list = line.split(" ")
-            # if len(my_list) > 1:
-            #     cls = my_list.pop(0)
-            #     obj = eval("{}({})".format(cls, temp3))
-            # else:
-            #     obj = eval("{}()".format(my_list[0]))
+            my_list = line.split(" ")
 
-            # parser = shlex.shlex(line, posix=True)
-            # parser.whitespace_split = True
-            # token = " "
-            # arg_list = []
-            # tok_list = []
-            # cls = parser.get_token()
-            # while token is not None:
-            #     token = parser.get_token()
-            #     tok_list.append(token)
-            # del tok_list[-1]
-            # print(tok_list)
-            # for arg in tok_list:
-            #     arg_list.append(arg.split('=')[0])
-            # for arg in tok_list:
-            #     arg_list.append(arg.split('=')[1])
-            #     temp = literal_eval(arg_list[0])
-
-            parser = shlex.shlex(line, posix=True)
-            parser.whitespace_split = True
-            token = " "
-            tok_list = []
-            tup_list = []
-            to_parse = {}
-            sanitized_args = {}
-            cls = parser.get_token()
-            while token is not None:
-                token = parser.get_token()
-                tok_list.append(token)
-            for tup in tok_list:
-                if tup is not None:
-                    tup_list.append(tup.partition('='))
-            for tuple_item in tup_list:
-                to_parse[tuple_item[0]] = tuple_item[2]
-            for k, v in to_parse.items():
-
-                # Matches string beginnig with 1 or more alphanum
-                # followed and ended by _id
-                if re.match("^\w+_id$", k):
-                    sanitized_args[k] = v
-
-                # Matches string that could be starting with + or -,
-                # followed by 1 to any amount of numbers
-                # followed by  a '.'
-                # followed by and anding with 1 to any amount of numbers
-                elif re.match("^[-+]?\d+\.\d+$", v):
-                    sanitized_args[k] = float(v)
-                elif v.isdigit() is True:
-                    sanitized_args[k] = int(v)
+            kwargs = {}
+            for i in range(1, len(my_list)):
+                key, value = tuple(my_list[i].split("="))
+                if value[0] == '"':
+                    value = value.strip('"').replace("_", " ")
                 else:
-                    if '_' in v:
-                        sanitized_args[k] = v.replace('_', ' ')
-                    else:
-                        sanitized_args[k] = v
-            obj = HBNBCommand.all_classes[cls](**sanitized_args)
+                    try:
+                        value = eval(value)
+                    except (SyntaxError, NameError):
+                        continue
+                kwargs[key] = value
+
+            if kwargs == {}:
+                obj = eval(my_list[0])()
+            else:
+                obj = eval(my_list[0])(**kwargs)
+                storage.new(obj)
+            print(obj.id)
             obj.save()
-            print("{}".format(obj.id))
+
         except SyntaxError:
             print("** class name missing **")
-        except KeyError:
+        except NameError:
             print("** class doesn't exist **")
 
     def do_show(self, line):
@@ -126,7 +86,7 @@ class HBNBCommand(cmd.Cmd):
             if not line:
                 raise SyntaxError()
             my_list = line.split(" ")
-            if my_list[0] not in self.all_classes:
+            if my_list[0] not in self.__classes:
                 raise NameError()
             if len(my_list) < 2:
                 raise IndexError()
@@ -157,7 +117,7 @@ class HBNBCommand(cmd.Cmd):
             if not line:
                 raise SyntaxError()
             my_list = line.split(" ")
-            if my_list[0] not in self.all_classes:
+            if my_list[0] not in self.__classes:
                 raise NameError()
             if len(my_list) < 2:
                 raise IndexError()
@@ -178,26 +138,21 @@ class HBNBCommand(cmd.Cmd):
             print("** no instance found **")
 
     def do_all(self, line):
-        """Prints all string representation of all instances
-        Exceptions:
-            NameError: when there is no object taht has the name
-        """
-
-        my_list = []
+        """Usage: all or all <class> or <class>.all()
+        Display string representations of all instances of a given class.
+        If no class is specified, displays all instantiated objects."""
         if not line:
-            objects = storage.all()
-            for key in objects:
-                my_list.append(objects[key])
-            print(my_list)
+            o = storage.all()
+            print([o[k].__str__() for k in o])
             return
         try:
             args = line.split(" ")
-            if args[0] not in self.all_classes:
+            if args[0] not in self.__classes:
                 raise NameError()
-            objects = storage.all(args[0])
-            for value in objects.values():
-                my_list.append(value)
-            print(my_list)
+
+            o = storage.all(eval(args[0]))
+            print([o[k].__str__() for k in o])
+
         except NameError:
             print("** class doesn't exist **")
 
@@ -215,7 +170,7 @@ class HBNBCommand(cmd.Cmd):
             if not line:
                 raise SyntaxError()
             my_list = split(line, " ")
-            if my_list[0] not in self.all_classes:
+            if my_list[0] not in self.__classes:
                 raise NameError()
             if len(my_list) < 2:
                 raise IndexError()
@@ -252,7 +207,7 @@ class HBNBCommand(cmd.Cmd):
         counter = 0
         try:
             my_list = split(line, " ")
-            if my_list[0] not in self.all_classes:
+            if my_list[0] not in self.__classes:
                 raise NameError()
             objects = storage.all()
             for key in objects:
--- models/amenity.py
+++ models/amenity.py
@@ -1,23 +1,23 @@
 #!/usr/bin/python3
-"""This is the amenity class"""
-import os
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, Integer
+"""Defines the Amenity class."""
+from models.base_model import Base
+from models.base_model import BaseModel
+from sqlalchemy import Column
+from sqlalchemy import String
 from sqlalchemy.orm import relationship
-from models.place import place_amenity
 
 
 class Amenity(BaseModel, Base):
-    """This is the class for Amenity
-    Attributes:
-        name: input name
-    """
+    """Represents an Amenity for a MySQL database.
 
-    __tablename__ = 'amenities'
-    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-        name = Column(String(128), nullable=False)
-        place_amenities = relationship('Place',
-                                       secondary=place_amenity,
-                                       back_populates='amenities')
-    else:
-        name = ""
+    Inherits from SQLAlchemy Base and links to the MySQL table amenities.
+
+    Attributes:
+        __tablename__ (str): The name of the MySQL table to store Amenities.
+        name (sqlalchemy String): The amenity name.
+        place_amenities (sqlalchemy relationship): Place-Amenity relationship.
+    """
+    __tablename__ = "amenities"
+    name = Column(String(128), nullable=False)
+    place_amenities = relationship("Place", secondary="place_amenity",
+                                   viewonly=False)
